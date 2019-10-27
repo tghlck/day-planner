68699005108315fdae31c96c440dc95c8f0c14e9
Adam Toomey <tghlck@gmail.com>
2019-10-26 21:49:38 -0400

    fleshed out hmtl, added styling and js for coloring text fields based on time and displays date/time at top

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+setInterval(getCurrentTime, 1000);
+
+//////////
+
+var currentDay = $("#currentDay");
+var date = $("<div>");
+
+//////////
+
+function getCurrentTime() {
+    date.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    currentDay.append(date);
+}
+
+//////////
+
+function liveTime() {
+    var today = new Date();
+    var currentHour = today.getHours();
+    console.log(
+        "current hour : " + currentHour);
+    for (var i = 9; i < 18; i++) {
+        if (i == currentHour) {
+            document.getElementById("t" + i).setAttribute("class", "present");
+        } else if (i < currentHour) {
+            document.getElementById("t" + i).setAttribute("class", "past");
+        } else if (i > currentHour) {
+            document.getElementById("t" + i).setAttribute("class", "future");
+        };
+    };
+};
+
+//////////
+
+function pgRdy() {
+    liveTime();
+};
