19df89479eac93c7579e1faf9c404e8ad007392d
Adam Toomey <tghlck@gmail.com>
2019-10-26 23:42:04 -0400

    having trouble getting the input fields to connect to the local storage, console logging onclicks

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,14 @@ var date = $("<div>");
 function getCurrentTime() {
     date.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
     currentDay.append(date);
-}
+};
 
 //////////
 
 function liveTime() {
     var today = new Date();
     var currentHour = today.getHours();
-    console.log(
-        "current hour : " + currentHour);
+    console.log("current hour : " + currentHour);
     for (var i = 9; i < 18; i++) {
         if (i == currentHour) {
             document.getElementById("t" + i).setAttribute("class", "present");
@@ -33,10 +32,25 @@ function liveTime() {
 //////////
 
 function pgRdy() {
+    event.preventDefault();
     liveTime();
     
 };
 
-function saveEntry() {
+//////////
 
+var entry = [];
+
+function getFromLocal(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
 };
+
+/////////
+
+function saveEntry(key, value) {
+    entry[key] = value;
+    alert("Entry Saved.");
+    console.log(key, value);
+    getFromLocal();
+};
+
